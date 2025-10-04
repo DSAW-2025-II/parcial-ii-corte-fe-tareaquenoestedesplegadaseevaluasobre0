6365c089e211908f29494a3113b3b7e80ea1857e
polosantini <polosantinimartin@gmail.com>
2025-10-03 21:45:53 -0500

    Error en config

--- js/config.js
+++ js/config.js
@@ -1,11 +1,9 @@
 const CONFIG = {
     BASE_URL: 'https://parcial-ii-corte-be-tareaquenoested.vercel.app/api/v1', // URL del backend desplegado
-
+    TOKEN_KEY: 'sessionToken', // Llave para almacenar el token en localStorage
     //Credenciales
     CREDENTIALS: {
         email: 'admin@admin.com',
         password: 'admin'
     }
 }
-//Guardado de la llave en el localStorage
-const TOKEN_KEY = 'sessionToken'
